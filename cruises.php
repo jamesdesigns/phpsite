e948110f037290fc3b58e3edee88cd000bd7bf7d
James Hooper <jameshooperdesigns@gmail.com>
2019-02-08 14:43:33 -0700

    Add Cruises title to the cruise page

--- cruises.php
+++ cruises.php
@@ -5,29 +5,30 @@ include '_top.php';
 
 
 <div class="cruiseContainer">
+    <h1>Cruises</h1>
     <div class="excursion">
         <img src="img/cruise1.jpg" class="destination1">
-        <h1>Seven Sins Yacht for charter from $1299</h1>
+        <h2>Seven Sins Yacht for charter from $1299</h2>
         <p class="cruiseInfo">Seven Sins's interior layout sleeps up to 10 guests in 5 rooms, including a master suite, 1 VIP stateroom and 2 double cabins. She is also capable of carrying up to 9 crew onboard to ensure a relaxed luxury yacht experience. Timeless styling, beautiful furnishings and sumptuous seating feature throughout to create an elegant and comfortable atmosphere.
 Her generous deck areas play host to a wide range of amenities including an outdoor bar, Jacuzzi and ample space for sunlounging and relaxing. Seven Sins's impressive leisure and entertainment facilities make her the ideal charter yacht for socialising and entertaining with family and friends.</p>
         <button>Read More</button>
     </div>
     <div class="excursion">
         <img src="img/cruise2.jpg" class="destination2">
-        <h1>Solo Yacht for charter from $1599</h1>
+        <h2>Solo Yacht for charter from $1599</h2>
         <p class="cruiseInfo">Solo's interior layout sleeps up to 12 guests in 6 rooms, including a master suite, 1 VIP stateroom, 2 double cabins and 2 twin cabins. She is also capable of carrying up to 18 crew onboard to ensure a relaxed luxury yacht experience. Timeless styling, beautiful furnishings and sumptuous seating feature throughout to create an elegant and comfortable atmosphere.
 Solo's impressive leisure and entertainment facilities make her the ideal charter yacht for socialising and entertaining with family and friends.</p>
         <button>Read More</button>
     </div>
     <div class="excursion">
         <img src="img/cruise3.jpg" class="destination3">
-        <h1>Alfa Nero yacht for charter from $2399</h1>
+        <h2>Alfa Nero yacht for charter from $2399</h2>
         <p class="cruiseInfo">Alfa Nero has a large infinity swimming pool with a glass aft end. The floor of the swimming pool can be raised to offer a safer depth for children – or raised completely to become either a dance floor or a helicopter landing pad.</p>
         <button>Read More</button>
     </div>
     <div class="excursion">
         <img src="img/cruise4.jpg" class="destination4">
-        <h1>Savannah Yacht for charter from $3499</h1>
+        <h2>Savannah Yacht for charter from $3499</h2>
         <p class="cruiseInfo">Savannah's interior layout sleeps up to 12 guests in 6 rooms, including a master suite, 1 VIP stateroom, 2 double cabins and 2 double/twin cabins. She is also capable of carrying up to 24 crew onboard to ensure a relaxed luxury yacht experience. Timeless styling, beautiful furnishings and sumptuous seating feature throughout to create an elegant and comfortable atmosphere.
 Savannah's impressive leisure and entertainment facilities make her the ideal charter yacht for socialising and entertaining with family and friends.</p>
         <button>Read More</button>
