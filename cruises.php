5fa642b3cf4a4c182d2d63ca5f60e693d52bd223
James Hooper <jameshooperdesigns@gmail.com>
2019-02-08 11:56:51 -0700

    Add content to the cruises page

--- cruises.php
+++ cruises.php
@@ -1,8 +1,31 @@
 <?php
-$title = "Home";
+$title = "Cruises";
 include '_top.php';
 ?>
 
+
+<div class="cruiseContainer">
+    <div class="excursion">
+        <img src="" class="destination1">
+        <p class="cruiseInfo"></p>
+        <button>Read More</button>
+    </div>
+    <div class="excursion">
+        <img src="" class="destination2">
+        <p class="cruiseInfo"></p>
+        <button>Read More</button>
+    </div>
+    <div class="excursion">
+        <img src="" class="destination3">
+        <p class="cruiseInfo"></p>
+        <button>Read More</button>
+    </div>
+    <div class="excursion">
+        <img src="" class="destination4">
+        <p class="cruiseInfo"></p>
+        <button>Read More</button>
+    </div>
+</div>
 
 
 
