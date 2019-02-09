e0925257dda8f189ca8a1c0a4255098060ab851b
James Hooper <jameshooperdesigns@gmail.com>
2019-02-09 13:37:47 -0700

    Add agent body of content

--- agentjay.php
+++ agentjay.php
@@ -3,8 +3,35 @@ $title = "Agent Jay Willardson";
 include '_top.php';
 ?>
 
-
 <div class="agents">
+ <div class="agentHeader">   
+<h1>Agent - Jay Willardson</h1>
+</div>
+<h2>Phone:</h2>
+<p>1-800-555-1234 ext 12</p>
+<br>
+<h2>Working:</h2>
+<p>Monday, Wednesday, Friday</p>
+<br>
+<h2>Destination Specialization</h2>
+<p>Israel, Egypt, Italy, Greece, Hawaii, Guatemala, Southern California, Orlando, Las Vegas, Bahamas, Caribbean cruises.</p>
+<br>
+<h2>Office Hours:</h2>
+<p>8:00am - 6:00pm</p>
+<br>
+<h2>Biography:</h2>
+<p>I have loved travel for as long as I can remember. At the age of 12 I asked if I could fly by myself to Hawaii and I never came back.</p>
+<p> Right out of high school I started into travel school and have had the opportunity to work in many aspects of the industry.
+    In every aspect I have worked I have had the opportunity to help people travel, to help fulfill people's dreams, what could 
+    be better to do every day!
+</p>
+<br>
+<h2>Customer Comment:</h2>
+<p>"Thank you Wendy for all your planning and efforts to ensure that we could enjoy our tour and bring back
+    such wonderful memories." -John Doe</p>
+<p>"Thank you for your cheery attitude and hard work during the planning of our tour!"  -The Smith's</p> 
+<p>"Thanks for your patience and efficiency. Let's do it again sometime very soon!" -Jane and John Westagar</p>  
+</div>
 
 
 
