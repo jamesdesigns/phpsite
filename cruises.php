e9fbccef0e15b5063011a7d5772983218d47ba3a
James Hooper <jameshooperdesigns@gmail.com>
2019-02-08 22:56:37 -0700

    Edit the cruise page according to the wireframes

--- cruises.php
+++ cruises.php
@@ -5,16 +5,16 @@ include '_top.php';
 
 
 <div class="cruiseContainer">
-    <h1>Cruises</h1>
+    <h1 class="title">Cruises</h1>
     <div class="excursion">
     <div class="block1">
         <img class="liner" src="img/cruise1.jpg">
     </div>
     <div class="block2">
         <h2>Seven Sins Yacht for charter from $1299</h2>
-        <p class="cruiseInfo">Seven Sins's interior layout sleeps up to 10 guests in 5 rooms, including a master suite, 1 VIP stateroom and 2 double cabins. She is also capable of carrying up to 9 crew onboard to ensure a relaxed luxury yacht experience. Timeless styling, beautiful furnishings and sumptuous seating feature throughout to create an elegant and comfortable atmosphere.
-Her generous deck areas play host to a wide range of amenities including an outdoor bar, Jacuzzi and ample space for sunlounging and relaxing. Seven Sins's impressive leisure and entertainment facilities make her the ideal charter yacht for socialising and entertaining with family and friends.</p>
-        <button>Read More</button>
+        <p class="cruiseInfo">Seven Sins's interior layout sleeps up to 10 guests in 5 rooms, including a master suite, 1 VIP stateroom and 2 double cabins. Timeless styling, beautiful furnishings and sumptuous seating feature throughout to create an elegant and comfortable atmosphere.
+        Seven Sins's impressive leisure and entertainment facilities make her the ideal charter yacht for socialising and entertaining with family and friends.</p>
+        <button class="button2">Read More</button>
         </div>
     </div>
     <div class="excursion">
@@ -23,9 +23,9 @@ Her generous deck areas play host to a wide range of amenities including an outd
         </div>
         <div class="block2">
         <h2>Solo Yacht for charter from $1599</h2>
-        <p class="cruiseInfo">Solo's interior layout sleeps up to 12 guests in 6 rooms, including a master suite, 1 VIP stateroom, 2 double cabins and 2 twin cabins. She is also capable of carrying up to 18 crew onboard to ensure a relaxed luxury yacht experience. Timeless styling, beautiful furnishings and sumptuous seating feature throughout to create an elegant and comfortable atmosphere.
+        <p class="cruiseInfo">Solo's interior layout sleeps up to 12 guests in 6 rooms, including a master suite, 1 VIP stateroom, 2 double cabins and 2 twin cabins. Timeless styling, beautiful furnishings and sumptuous seating feature throughout to create an elegant and comfortable atmosphere.
 Solo's impressive leisure and entertainment facilities make her the ideal charter yacht for socialising and entertaining with family and friends.</p>
-        <button>Read More</button>
+        <button class="button2">Read More</button>
         </div>
     </div>
     <div class="excursion">
@@ -35,7 +35,7 @@ Solo's impressive leisure and entertainment facilities make her the ideal charte
         <div class="block2">
         <h2>Alfa Nero yacht for charter from $2399</h2>
         <p class="cruiseInfo">Alfa Nero has a large infinity swimming pool with a glass aft end. The floor of the swimming pool can be raised to offer a safer depth for children – or raised completely to become either a dance floor or a helicopter landing pad.</p>
-        <button>Read More</button>
+        <button class="button2">Read More</button>
         </div>
     </div>
     <div class="excursion">
@@ -44,9 +44,9 @@ Solo's impressive leisure and entertainment facilities make her the ideal charte
         </div>
         <div class="block2">
         <h2>Savannah Yacht for charter from $3499</h2>
-        <p class="cruiseInfo">Savannah's interior layout sleeps up to 12 guests in 6 rooms, including a master suite, 1 VIP stateroom, 2 double cabins and 2 double/twin cabins. She is also capable of carrying up to 24 crew onboard to ensure a relaxed luxury yacht experience. Timeless styling, beautiful furnishings and sumptuous seating feature throughout to create an elegant and comfortable atmosphere.
+        <p class="cruiseInfo">Savannah's interior layout sleeps up to 12 guests in 6 rooms, including a master suite, 1 VIP stateroom, 2 double cabins and 2 double/twin cabins. Timeless styling, beautiful furnishings and sumptuous seating feature throughout to create an elegant and comfortable atmosphere.
 Savannah's impressive leisure and entertainment facilities make her the ideal charter yacht for socialising and entertaining with family and friends.</p>
-        <button>Read More</button>
+        <button class="button2">Read More</button>
         </div>
     </div>
 </div>
